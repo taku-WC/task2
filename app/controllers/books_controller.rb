f69f5e320476a42ba92f83b8a19df6e6760cae68
taku-WC <pio.neer.bi10@gmail.com>
2020-07-13 09:53:55 +0900

    [start&finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	@book = Book.new #空のbookを作る
+  end
+
+  def create
+    @book = Book.new(book_params) #入力された情報を使ってbookを作る
+    @books = Book.all
+    if @book.save
+    	flash[:success_notice] = 'successfully!'
+		redirect_to book_path(@book.id)
+	else
+	    render 'index'
+	end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+	@book = Book.find(params[:id])
+	if @book.update(book_params)
+		flash[:success_notice] = 'successfully'
+		redirect_to book_path(@book.id)
+	else
+		render 'edit'
+	end
+  end
+
+  def top
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :books
+
+	root 'books#top'
+end
